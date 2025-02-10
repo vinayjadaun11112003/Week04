2d5d25a75aa9a8d4ce77dcdbd62c487b5846b766
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-02-10 13:35:48 +0530

    Initiallizing Week04 Repository

---
+++ First.java
@@ -0,0 +1,6 @@
+public class First {
+    public static void main(String[] args){
+        System.out.println("This repository contains Week04 submittion files of capgemini-CG Training.");
+        System.out.println("Happy Coding :) ");
+    }
+}
