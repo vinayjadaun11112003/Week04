63c27efaf9766305e73b71211ef8a0537892f161
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-02-15 16:50:57 +0530

    [Vinay Jadaun] - Add. extract currency value from the string

---
+++ Day05/com/extract_currency_value_13/ExtractCurrencyValue.java
@@ -0,0 +1,51 @@
+package com.extract_currency_value_13;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//ExtractCurrencyValue --> This class is created for the extraction of currency value from the string.
+public class ExtractCurrencyValue {
+    //color string to give color to the strings.
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    //method which return the arraylist of type string after the extraction of currency value from string.
+    public static ArrayList<String> extract(String regex, String input){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        ArrayList<String> st = new ArrayList<>();
+        while(matcher.find()){
+            st.add(matcher.group());
+        }
+        return st;
+
+    }
+
+    //Main method to control the flow of code.
+    public static void main(String[] args){
+        boolean flag = true;
+        String regex = "^[$]+{1}[0-9.]+";
+        System.out.println(ANSI_CYAN+"EXTRACT CURRENCY VALUE FROM STRING SYSTEM(if you want to exit the program type 'exit')"+ANSI_RESET);
+        while(flag) {
+            Scanner input = new Scanner(System.in);
+            System.out.print(ANSI_WHITE+"Enter the String : "+ANSI_RESET);
+            String userInput = input.nextLine();
+            if(userInput.equals("exit")){
+                flag = false;
+                System.out.println(ANSI_WHITE+"Exiting the program..."+ANSI_RESET);
+            }else{
+                ArrayList<String> ans = extract(regex,userInput);
+                for(String a : ans){
+                    System.out.println(ANSI_CYAN+a+ANSI_RESET);
+                }
+            }
+        }
+
+    }
+}
+
+
